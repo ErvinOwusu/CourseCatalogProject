12d0dd1761b7ab06d3b284a47e95cd1b09772b3f
ErvinOwusu <ervin.owusu@student.region16ct.org>
2023-02-01 19:07:16 -0500

    English work

---
+++ JAVA/formatLearningGoal.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class formatLearningGoal {
+    public static void main(String[] args) {
+        String text = "- communicate ideas and information coherently in a variety of circumstances. - read actively and critically for a variety of purposes. - apply the conventions of grammar for formative and summative writing. - listen actively, critically, and responsibly. - comprehend and analyze culturally diverse literature. - explain how different cultures express, communicate, and instill their most important ideas. - be introduced to the skills necessary to be successful on standardized tests. - use the language arts to explore and respond to classical texts from many cultures and historical periods. - Acquire, analyze and apply text-base"
+
+        while (true)
+        {    
+            System.out.println(substring(indexOf("-"), indexOf(" - ")));
+        }
+    }
+}
---
+++ SCRIPTS/englishScript.js
@@ -0,0 +1,96 @@
+var more113 = document.getElementById('more113');
+var more123 = document.getElementById('more123');
+var more133 = document.getElementById('more133');
+var more138 = document.getElementById('more138');
+var more139 = docuemnt.getElementById('more139');
+var more190 = document.getElementsByClassName('more190');
+
+var learningGoalsBackground = document.getElementById('learningGoalsBackground');
+
+function learningGoals113() {
+    if (more113.style.display === "inline")
+    {
+        more113.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+        
+    }
+    else
+    {
+        more113.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals123() {
+    if (more123.style.display === "inline")
+    {
+        more123.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+        
+    }
+    else
+    {
+        more123.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals133() {
+    if (more133.style.display === "inline")
+    {
+        more133.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+        
+    }
+    else
+    {
+        more133.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals138() {
+    if (more138.style.display === "inline")
+    {
+        more138.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+        
+    }
+    else
+    {
+        more138.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals139() {
+    if (more139.style.display === "inline")
+    {
+        more139.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+        
+    }
+    else
+    {
+        more139.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals190() {
+    if (more190.style.display === "inline")
+    {
+        more190.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+        
+    }
+    else
+    {
+        more190.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function test() {
+    more190.style.color = "red";
+}
