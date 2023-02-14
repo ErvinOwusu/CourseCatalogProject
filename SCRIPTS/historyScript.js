6b82fd0fbbc91cf2e0ae2b3ab2bb76f13a4d0743
ErvinOwusu <ervin.owusu@student.region16ct.org>
2023-02-13 22:15:34 -0500

    Started work on history

---
+++ SCRIPTS/historyScript.js
@@ -0,0 +1,41 @@
+var learningGoalsBackground = document.getElementById('learningGoalsBackground');
+
+function learningGoals013(){
+    if (more013.style.display === "inline" )
+    {
+        more013.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else
+    {
+        more013.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals023() {
+    if (more023.style.display === "inline" )
+    {
+        more023.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else
+    {
+        more023.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals033() {
+    if (more033.style.display === "inline" )
+    {
+        more033.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else
+    {
+        more033.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
