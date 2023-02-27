9d715a63b76ca0a5b8cfa471a3a5bbb0db9e197d
ErvinOwusu <ervin.owusu@student.region16ct.org>
2023-02-26 22:15:21 -0500

    Working grids for social studies

--- SCRIPTS/historyScript2.js
+++ SCRIPTS/historyScript2.js
@@ -1,4 +1,9 @@
+/*
+var grid013 = document.getElementById('grid013');
+var grid023 = document.getElementById('grid023');
+var grid033 = document.getElementById('grid033');
 var grid042 = document.getElementById('grid042');
+
 var grid058 = document.getElementById('grid058');
 var grid036 = document.getElementById('grid036');
 var grid062 = document.getElementById('grid062');
@@ -9,7 +14,6 @@ var grid055 = document.getElementById('grid055');
 var grid049 = document.getElementById('grid049');
 var grid726 = document.getElementById('grid726');
 
-var check4 = document.getElementById('check4');
 
 function display2() {
     if (check4.checked == true) {
@@ -25,6 +29,55 @@ function display2() {
         grid726.style.width = "50%";
     }
     else {
+        grid042.style.width = "100%";
+        grid058.style.width = "100%";
+        grid036.style.width = "100%";
+        grid062.style.width = "100%";
+        grid052.style.width = "100%";
+        grid046.style.width = "100%";
+        grid040.style.width = "100%";
+        grid055.style.width = "100%";
+        grid049.style.width = "100%";
+        grid726.style.width = "100%";
+    }
+}
+*/
+
+var grid013 = document.getElementById('grid013');
+var grid023 = document.getElementById('grid023');
+var grid033 = document.getElementById('grid033');
+var grid042 = document.getElementById('grid042');
+var grid058 = document.getElementById('grid058');
+var grid036 = document.getElementById('grid036');
+var grid062 = document.getElementById('grid062');
+var grid052 = document.getElementById('grid052');
+var grid046 = document.getElementById('grid046');
+var grid040 = document.getElementById('grid040');
+var grid055 = document.getElementById('grid055');
+var grid049 = document.getElementById('grid049');
+var grid726 = document.getElementById('grid726');
+
+function display2() {
+    if (check4.checked == true) {
+        grid013.style.width = "50%";
+        grid023.style.width = "50%";
+        grid033.style.width = "50%";
+        grid042.style.width = "50%";
+        grid058.style.width = "50%";
+        grid036.style.width = "50%";
+        grid062.style.width = "50%";
+        grid052.style.width = "50%";
+        grid046.style.width = "50%";
+        grid040.style.width = "50%";
+        grid055.style.width = "50%";
+        grid049.style.width = "50%";
+        grid726.style.width = "50%";
+
+    }
+    else {
+        grid013.style.width = "100%";
+        grid023.style.width = "100%";
+        grid033.style.width = "100%";
         grid042.style.width = "100%";
         grid058.style.width = "100%";
         grid036.style.width = "100%";
