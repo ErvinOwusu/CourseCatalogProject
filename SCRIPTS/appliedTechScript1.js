2fdb7f3147b34e7c30ea08465b5ec55c90d4da7b
ErvinOwusu <ervin.owusu@student.region16ct.org>
2023-03-20 07:41:59 -0400

    Forgot to add script files

--- SCRIPTS/appliedTechScript1.js
+++ SCRIPTS/appliedTechScript1.js
@@ -118,3 +118,168 @@ function learningGoals728() {
         learningGoalsBackground.style.padding = "2px 2px 15px 20px";
     }
 }
+
+function learningGoals767() {
+    if (more767.style.display === "inline") {
+        more767.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more767.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals761() {
+    if (more761.style.display === "inline") {
+        more761.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more761.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals766() {
+    if (more766.style.display === "inline") {
+        more766.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more766.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals764() {
+    if (more764.style.display === "inline") {
+        more764.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more764.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals762() {
+    if (more762.style.display === "inline") {
+        more762.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more762.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals763() {
+    if (more763.style.display === "inline") {
+        more763.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more763.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals786() {
+    if (more786.style.display === "inline") {
+        more786.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more786.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals787() {
+    if (more787.style.display === "inline") {
+        more787.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more787.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals779() {
+    if (more779.style.display === "inline") {
+        more779.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more779.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals797() {
+    if (more797.style.display === "inline") {
+        more797.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more797.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals798() {
+    if (more798.style.display === "inline") {
+        more798.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more798.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals798() {
+    if (more798.style.display === "inline") {
+        more798.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more798.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals784() {
+    if (more784.style.display === "inline") {
+        more784.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more784.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals794() {
+    if (more794.style.display === "inline") {
+        more794.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more794.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
+
+function learningGoals704() {
+    if (more704.style.display === "inline") {
+        more704.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more704.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
