1c133c0d545a7be5b03e8b139ae0cc10a236f8f7
ErvinOwusu <ervin.owusu@student.region16ct.org>
2023-03-20 18:07:49 -0400

    adding other edited files

---
+++ SCRIPTS/appliedTechScript2.js
@@ -0,0 +1,40 @@
+
+
+function hideBusinessCourses() {
+    if (businessEducation.style.display == "none")
+    {
+        businessEducation.style.display = "block";
+        businessEducation2.style.display = "block";
+    }
+    else
+    {
+        businessEducation.style.display = "none";
+        businessEducation2.style.display = "none";
+    }
+}
+
+function hideFamilyCourses() {
+    if (family.style.display == "none")
+    {
+        family.style.display = "block";
+        family2.style.display = "block";
+    }
+    else
+    {
+        family.style.display = "none";
+        family2.style.display = "none";
+    }
+}
+
+function hideTechnologyCourses() {
+    if (technology.style.display == "none")
+    {
+        technology.style.display = "block";
+        technology2.style.display = "block";
+    }
+    else
+    {
+        technology.style.display = "none";
+        technology2.style.display = "none";
+    }
+}
