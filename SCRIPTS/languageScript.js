171c009f666a49130bb00ab36994048a98098252
ErvinOwusu <ervin.owusu@student.region16ct.org>
2023-04-13 19:27:38 -0400

    Began work on world language

---
+++ SCRIPTS/languageScript.js
@@ -0,0 +1,10 @@
+function learningGoals100() {
+    if (more100.style.display === "inline") {
+        more100.style.display = "none";
+        learningGoalsBackground.style.padding = "2px 2px 2px 20px";
+    }
+    else {
+        more100.style.display = "inline";
+        learningGoalsBackground.style.padding = "2px 2px 15px 20px";
+    }
+}
---
+++ SCRIPTS/languageScript2.js
@@ -0,0 +1,12 @@
+function hideRosetta() {
+    if (rosetta.style.display == "none")
+    {
+        rosetta.style.display = "block";
+        rosetta2.style.display = "block";
+    }
+    else
+    {
+        rosetta.style.display = "none";
+        rosetta2.style.display = "none";
+    }
+}
